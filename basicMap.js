0e1f7353d06eda05198ccf232999dcf768abcbe2
jcbalt <jalt1446@gmail.com>
2024-07-11 16:34:07 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,5 +3,6 @@ console.log("basicMap.js loaded.");
 // CPCC Coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
 
 let map = L.map("map", {
-center: [35.2176665, -80.831473]
+center: [35.2176665, -80.831473],
+  zoom: 16
 });
