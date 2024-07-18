82eaf4a5860ceb79297c61ae6d8476cba3ad7878
jcbalt <jalt1446@gmail.com>
2024-07-18 15:32:40 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -2,10 +2,6 @@ console.log("basicMap.js loaded.");
 
 // CPCC Coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
 
-let map = L.map("map", {
-center: [35.2176665, -80.831473],
-  zoom: 16
-});
 
 
 let markers = [];
@@ -46,3 +42,8 @@ let baseMaps = {
 let overlayMaps = {
 	 CPCC:CPCC
 }
+
+let map = L.map("map", {
+center: [35.2176665, -80.831473],
+  zoom: 16
+});
